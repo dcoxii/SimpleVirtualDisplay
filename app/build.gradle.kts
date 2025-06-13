d8553c3e1cbd7e9c1d4a08e89bbebbb842e96eac
superman6462 <easniton@gmail.com>
2025-06-13 23:35:26 +0600

    Update bi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.kangrio.virtualdisplay"
-        minSdk = 23
+        minSdk = 28
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
