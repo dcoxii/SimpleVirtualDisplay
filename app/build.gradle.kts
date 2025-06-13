41124b76b8efb0a10f54176d47ef45465f6b9240
superman6462 <easniton@gmail.com>
2025-06-13 23:04:33 +0600

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.kangrio.virtualdisplay"
-    compileSdk = 34
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "com.kangrio.virtualdisplay"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
